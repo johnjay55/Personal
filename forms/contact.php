53777a42dbfe0f5a683ca52b0b34618012391548
johnjay55 <johnjaykaraan55@gmail.com>
2023-11-07 16:21:11 +0800

    test

--- forms/contact.php
+++ forms/contact.php
@@ -3,7 +3,7 @@
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the template
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
+  
   */
 
   // Replace contact@example.com with your real receiving email address
@@ -26,7 +26,7 @@
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
  
   $contact->smtp = array(
-    'host' => 'johnjaykaraan55@gmail.com ',
+    'host' => 'smtp.gmail.com ',
     'username' => 'johnjay55',
     'password' => 'pass',
     'port' => '587'
